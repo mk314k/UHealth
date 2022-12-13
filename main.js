a5f4ad21780d41f2ffc3215b6e1f79b39b6f4093
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2022-12-13 02:54:57 -0500

    updating index

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
+if (signUpButton){
+    signUpButton.addEventListener('click', () =>
+    container.classList.add('right-panel-active'));
+}
+if (signInButton){
+    signInButton.addEventListener('click', () =>
+    container.classList.remove('right-panel-active'));
+}
 
-signInButton.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));
+
 
 console.log("Console works!");
